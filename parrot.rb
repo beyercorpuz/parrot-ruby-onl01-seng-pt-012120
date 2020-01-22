a452f7e1e879f7c6524331562a11ac763665837b
Chris Beyer <beyercorpuz@gmail.com>
2020-01-22 22:43:41 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,5 +1,5 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def #introduction_with_language(name,language)
-  puts "Hi, my name is #{name} and I am learning to program in #{language}"
+def #introduction_with_cutie("Cutie","Squawk!"")
+  puts "Hi, my name is #{Cutie} and I am learning to #{Squawk}"
 end
